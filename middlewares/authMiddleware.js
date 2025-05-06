a0093c866a61bcda11c148b60194b384000b1eae
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-05-06 10:39:54 +0300

    feat: Add comments to auth middleware for improved clarity and maintainability

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,13 @@
 const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 
+/**
+ * Middleware funkcija autentifikavimui (tikrina JWT tokeną ir prideda vartotojo objektą prie `req`)
+ */
 const authMiddleware = async (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "")
 
+  // Jei nėra tokeno – grąžinamas 401 klaidos atsakymas
   if (!token) {
     return res
       .status(401)
@@ -13,14 +17,19 @@ const authMiddleware = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    const user = await User.findById(decoded.id).select("-password")
+    // Gaunamas vartotojas pagal tokeno ID
+    const user = await User.findById(decoded.id).select("-password") 
+
+    // Jei vartotojas nerastas – klaida
     if (!user) {
       return res.status(401).json({ message: "User not found" })
     }
 
     req.user = user 
+
     next()
   } catch (error) {
+    // Tokenas netinkamas ar pasibaigęs
     res.status(400).json({ message: "Invalid token", error })
   }
 }
