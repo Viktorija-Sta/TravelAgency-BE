5cbd5da1a676e006ccace5c03f5ad074e40d0d3f
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-05-08 12:31:49 +0300

    refactor: Restructure index.js for improved readability and organization; move mongoose connection and API routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const process = require("process");
 require("dotenv").config();
-require("./db");
+const mongoose = require("mongoose");
 const globalErrorHandler = require("./controllers/errorController");
 
 const bodyParser = require("body-parser");
@@ -10,15 +10,14 @@ const cors = require("cors");
 
 const app = express();
 
-// CORS nustatymai, leidžiantys visus Vercel subdomenus
+// CORS nustatymai
 const allowedOrigins = [
   "https://atsiskaitymas-fe-3tpy.vercel.app",
-  /\.vercel\.app$/  // leidžia visus Vercel subdomenus
+  /\.vercel\.app$/
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Jei kilmė (origin) nėra nurodyta (lokali aplinka) arba atitinka leidžiamus domenus
     if (!origin || allowedOrigins.some((allowedOrigin) => origin.match(allowedOrigin))) {
       callback(null, true);
     } else {
@@ -31,34 +30,44 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// API maršrutai
-const userAPIRoutes = require('./api/users');
-const categoryAPIRoutes = require('./api/categoryRoutes');
-const adminAPIRoutes = require('./api/adminRoutes');
-const agencyAPIRoutes = require('./api/agencyRoutes');
-const destinationAPIRoutes = require('./api/destinationRoutes');
-const hotelAPIRoutes = require('./api/hotelRoutes');
-const orderAPIRoutes = require('./api/orders');
-const reviewAPIRoutes = require('./api/reviewRoutes');
+// Duomenų bazės prisijungimas
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
 
-app.use('/api/users', userAPIRoutes);
-app.use('/api/categories', categoryAPIRoutes);
-app.use('/api/admin', adminAPIRoutes);
-app.use('/api/agencies', agencyAPIRoutes);
-app.use('/api/destinations', destinationAPIRoutes);
-app.use('/api/hotels', hotelAPIRoutes);
-app.use('/api/reviews', reviewAPIRoutes);
-app.use('/api/orders', orderAPIRoutes);
+    // API maršrutai
+    const userAPIRoutes = require('./api/users');
+    const categoryAPIRoutes = require('./api/categoryRoutes');
+    const adminAPIRoutes = require('./api/adminRoutes');
+    const agencyAPIRoutes = require('./api/agencyRoutes');
+    const destinationAPIRoutes = require('./api/destinationRoutes');
+    const hotelAPIRoutes = require('./api/hotelRoutes');
+    const orderAPIRoutes = require('./api/orders');
+    const reviewAPIRoutes = require('./api/reviewRoutes');
 
-app.use(globalErrorHandler);
+    app.use('/api/users', userAPIRoutes);
+    app.use('/api/categories', categoryAPIRoutes);
+    app.use('/api/admin', adminAPIRoutes);
+    app.use('/api/agencies', agencyAPIRoutes);
+    app.use('/api/destinations', destinationAPIRoutes);
+    app.use('/api/hotels', hotelAPIRoutes);
+    app.use('/api/reviews', reviewAPIRoutes);
+    app.use('/api/orders', orderAPIRoutes);
 
-// Pagrindinis maršrutas
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+    app.use(globalErrorHandler);
 
-// Serverio paleidimas
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    // Pagrindinis maršrutas
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+
+    // Serverio paleidimas
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB:", error);
+  });
