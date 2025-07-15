61087e5925d0c1709521dc1cadf7c95f3454823c
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-07-15 17:49:39 +0300

    fix: Update default server port from 3001 to 2001

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 2001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
